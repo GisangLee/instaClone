b6ab95d9e89f979b82f53807b38fad9eb501d130
DaeLee <masterkorea01@naver.com>
2020-06-17 11:07:54 +0900

    Follow style

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.conf.urls.static import static
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 from django_pydenticon.views import image as pydenticon_image
 
 
@@ -18,9 +18,9 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('identicon/image/<path:data>/',
          pydenticon_image, name='pydenticon_image'),
-    path('', include('instagram.urls')),
-    path('', login_required(TemplateView.as_view(
-        template_name="root.html")), name="root"),
+    path('instagram/', include('instagram.urls')),
+    path('', login_required(RedirectView.as_view(
+        pattern_name="instagram:index")), name="root"),
 ]
 
 if settings.DEBUG:
